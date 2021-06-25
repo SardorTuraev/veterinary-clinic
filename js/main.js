2337e1aaec97d4df8c8cfd4acf6f2160922dc813
SardorTuraev <sardortura2255@gmail.com>
2021-06-25 08:59:46 +0500

    smth

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// Modalni ochuvchi hamma elementlarni topiladi
+// Modal topiladi
+// Modalni yopuvchi element topiladi
+
+// Modalni ochuvchi funksiya (modalga mos klassni qo'shadi)
+
+
+// Modalni yopuvchi funksiya (modaldan mos klassni olib tashlaydi)
+
+
+// Agar modalni ochuvchi tugmalar bo'lsa
+  // Ularning har biriga link deb murojaat qiladi
+    // va har biriga click hodisasida
+      // tabiiy reaksiyani to'xtatib
+      // Modalni ochish funksiyasini ishga tushiradi
+
+
+// Agar modalni yopuvchi tugma bo'lsa
+  // U tugmaga click hodisasida
+    // Modalni yopish funksiyasini ishga tushirishni buyuradi
+
+
+
+
 var elOpenModalButton = document.querySelector('.js-open-modal-button');
 var elCloseModalButton = document.querySelector('.js-close-modal-button');
 var elModal = document.querySelector('.js-modal');
